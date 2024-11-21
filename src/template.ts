2414b7581bb0e438f4f9584e4f719021b1aa8647
johanbuis <johan.buis@ns.nl>
2024-11-21 14:57:01 +0100

    test: move one space to the right

--- src/template.ts
+++ src/template.ts
@@ -1,3 +1,6 @@
 export function escapeCarpark(carPark: number[][]): string[] {
+  if (carPark[0].length > 1) {
+    return ["R1"];
+  }
   return [];
 }
--- test/unit/unittemplate.test.ts
+++ test/unit/unittemplate.test.ts
@@ -6,4 +6,9 @@ describe("We need to escape the car park", () => {
     const expectedRoute: string[] = [];
     expect(escapeCarpark(carPark)).toEqual(expectedRoute);
   });
+  it("The car park is a bit bigger, but now we need to move one space to the right to exit", () => {
+    const carPark = [[2, 0]];
+    const expectedRoute: string[] = ["R1"];
+    expect(escapeCarpark(carPark)).toEqual(expectedRoute);
+  });
 });
