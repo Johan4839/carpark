5fa46ad4fc7683cee825f9dc531d9b8d4fcb4bc3
johanbuis <johan.buis@ns.nl>
2024-12-20 15:33:41 +0100

    R: one constant for ground floor length

--- src/template.ts
+++ src/template.ts
@@ -1,9 +1,9 @@
 function calculateStepsRight(
-  exitPosition: number,
+  positionToGoTo: number,
   currentPosition: number,
   route: string[]
 ) {
-  const stepsRight = exitPosition - currentPosition;
+  const stepsRight = positionToGoTo - currentPosition;
 
   if (stepsRight > 0) {
     route.push(`R${stepsRight}`);
@@ -11,23 +11,21 @@ function calculateStepsRight(
 }
 export function escapeCarpark(carPark: number[][]): string[] {
   const route: string[] = [];
+  const groundFloorExitPosition = carPark[0].length - 1;
   if (carPark.length === 1) {
     const currentPosition = carPark[0].indexOf(2);
-    const exitPosition = carPark[0].length - 1;
-    calculateStepsRight(exitPosition, currentPosition, route);
+    calculateStepsRight(groundFloorExitPosition, currentPosition, route);
   }
 
   if (carPark.length === 2) {
     const firstFloor = carPark[0];
-    const secondFloor = carPark[1];
     const currentPosition = firstFloor.indexOf(2);
     const stairCasePosition = firstFloor.indexOf(1);
     calculateStepsRight(stairCasePosition, currentPosition, route);
 
     route.push("D1");
 
-    const secondFloorExitPosition = secondFloor.length - 1;
-    calculateStepsRight(secondFloorExitPosition, stairCasePosition, route);
+    calculateStepsRight(groundFloorExitPosition, stairCasePosition, route);
   }
 
   return route;
